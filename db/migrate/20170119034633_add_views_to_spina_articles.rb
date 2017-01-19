9c932e22d6ad178807b30055dcc20368d4fd8922
Phong Phan <fongfan999@gmail.com>
2017-01-19 15:23:13 +0700

    Fixing type in migration

--- db/migrate/20170119034633_add_views_to_spina_articles.rb
+++ db/migrate/20170119034633_add_views_to_spina_articles.rb
@@ -1,5 +1,5 @@
 class AddViewsToSpinaArticles < ActiveRecord::Migration[5.0]
   def change
-    add_column :spina_articles, :views, :interger, default: 0
+    add_column :spina_articles, :views, :integer, default: 0
   end
 end
